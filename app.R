41e173c55dd4cb158530b3638241cf1066bd62f0
Theodore Lytras <thlytras@gmail.com>
2020-03-07 18:39:14 +0200

    Add social distancing

--- app.R
+++ app.R
@@ -27,7 +27,7 @@ ui <- fluidPage(
       sliderInput("cIntros", "Επιπλέον εισαγωγές μολυσματικών ανά ημέρα", 
                   min=0, max=20, value=0, step=1),
       dateInput("cStopDate", "Προβολή επιδημίας έως", value="2020-10-1", min="2020-5-1", max="2021-7-1", format="dd/mm/yyyy"),
-      checkboxInput("sCntTrc", "Contact tracing"),
+      checkboxInput("sCntTrc", "Διερεύνηση επαφών"),
       conditionalPanel("input.sCntTrc", 
         wellPanel(
           sliderInput("cAsc", "Ascertainment ratio (%)", min=0, max=50, value=10),
@@ -37,6 +37,24 @@ ui <- fluidPage(
           sliderInput("cMaxCnt", "Μέγιστος αριθμός επαφών που μπορούν να διερευνηθούν και να απομονωθούν αποτελεσματικά ανά ημέρα",
             min=0, max=1000, value=0, step=5),
           helpText("Θέσατε 0 αν νομίζετε οτι το contact tracing μπορεί να κλιμακωθεί επ'αόριστον.")
+        )
+      ),
+      checkboxInput("sSocDis", "Κοινωνική αποστασιοποίηση"),
+      conditionalPanel("input.sSocDis", 
+        wellPanel(
+          sliderInput("cSympPct", "Ποσοστό (%) πλήρους απομόνωσης συμπτωματικών",
+            min=0, max=100, value=0, step=1),
+          sliderInput("cSympDelay", "Απομόνωση συμπτωματικών μετά από πόσες μέρες από εμφάνιση συμπτωμάτων?",
+            min=0, max=3, value=1),
+          fluidRow(
+            column(8, sliderInput("cBetaDec", "Ποσοστιαία (%) μείωση contact rate", min=0, max=50, value=0)),
+            column(4, dateInput("cBetaDecDate", "Από πότε?", value="2020-4-1", min="2020-3-1", max="2021-5-1", format="dd/mm/yyyy"))
+          ),
+          fluidRow(
+            column(8, sliderInput("cSchoolEff", "Ποσοστό (%) \"ανοσοποίησης\" από κλείσιμο σχολείων", min=0, max=100, value=0)),
+            column(4, dateInput("cSchoolDate", "Κλείσιμο σχολείων πότε?", value="2020-4-1", min="2020-3-1", max="2021-5-1", format="dd/mm/yyyy"))
+          ),
+          helpText("Θεωρούμε (πολύ απλουστευτικά) οτι από τα ~2 εκατ. μαθητών/φοιτητών ένα ποσοστό αφαιρείται από το pool των επίνοσων.")
         )
       )
     ),
@@ -74,18 +92,27 @@ server <- function(input, output, session) {
     a$I[1] <- input$cIni
     minD <- as.integer(format(input$cMinD, "%j"))
     a$beta <- ((cos((as.integer(format(a$date, "%j"))-(minD-183))*2*pi/366)+1)/2*(R0-R0min) + R0min)/D
+    a$beta[which(a$date >= input$cBetaDecDate)] <- a$beta[which(a$date >= input$cBetaDecDate)] * (100 - input$cBetaDec)/100
     kappa <- 1/L
-    gamma <- 1/D
+#    gamma <- 1/D
+    gamma <- 1/(D - max(0, D-input$cSympDelay)*input$cSympPct/100)
     for (t in 2:nrow(a)) {
       a$dE[t] <- round((1 - exp(-a$beta[t]*a$I[t-1]/a$S[1]))*a$S[t-1])
       a$dI[t] <- round((1 - exp(-kappa))*a$E[t-1])
       a$dR[t] <- round((1 - exp(-gamma))*a$I[t-1])
+      # Control measures
+        # How many exposed contacts we trace and isolate (remove)
         cntRemoved <- round(input$cPctCnt*input$cAsc/100/100*a$dE[t])
         if (input$cMaxCnt!=0 & input$cMaxCnt<cntRemoved) cntRemoved <- input$cMaxCnt
       a$E[t] <- a$E[t-1] + a$dE[t] - a$dI[t] - cntRemoved
-      a$I[t] <- a$I[t-1] + a$dI[t] - a$dR[t] + input$cIntros
-      a$R[t] <- a$R[t-1] + a$dR[t] + cntRemoved
+      a$I[t] <- a$I[t-1] + a$dI[t] - a$dR[t] + input$cIntros 
+      a$R[t] <- a$R[t-1] + a$dR[t] + cntRemoved 
       a$S[t] <- a$S[1] - a$E[t] - a$I[t] - a$R[t]
+      if (a$date[t]==input$cSchoolDate) {
+        mv <- round(a$S[t]*2/11*input$cSchoolEff/100)
+        a$S[t] <- a$S[t] - mv
+        a$R[t] <- a$R[t] + mv
+      }
     }
     return(a)
   })
